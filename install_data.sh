87733c5d51f67cc4eb87a3b5ac16ad32d6269f38
Faisal Jayousi <fjayousi90@gmail.com>
2024-05-26 14:18:13 +0200

    Delete install_data.sh

--- install_data.sh
+++
@@ -1,9 +0,0 @@
-echo "Now installing data from Kaggle."
-
-!pip install -q kaggle
-
-!mkdir ~/.kaggle
-! mv kaggle.json ~/.kaggle/
-! chmod 600 ~/.kaggle/kaggle.json
-!kaggle datasets download meowmeowmeowmeowmeow/gtsrb-german-traffic-sign
-!unzip -qq gtsrb-german-traffic-sign.zip
